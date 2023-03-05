76732ca92fd47f4a37043e01155a8305634aee9c
danielchoike2 <89948222+danielchoike2@users.noreply.github.com>
2023-03-05 15:29:11 -0500

    Add project files.

---
+++ GenericAnimalClass/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+// define a generics class named Animal
+public class Animal<T>
+{
+    // define a field of type T 
+    public T data;
+
+    // define a constructor of the Animal class 
+    public Animal(T data)
+    {
+        this.data = data;
+        Console.WriteLine("Animal Data:" + this.data);
+    }
+
+    public T getAnimal()
+    {
+        return data;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // create an instance with a string data type
+        Animal<string> animalName = new Animal<string>("Boogie");
+
+        // create an instance with another string data type
+        Animal<string> animalHabitat = new Animal<string>("Amazon River");
+
+        Animal<bool> endangered = new Animal<bool>(false);
+
+        Animal<bool> extinct = new Animal<bool>(false);
+
+        Animal<int> averageLifespan = new Animal<int>(10);
+
+    } 
+}
+
+
+
+
+
